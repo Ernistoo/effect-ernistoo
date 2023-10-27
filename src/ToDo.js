fdf373a6a3779e98d28ee26efc928bcd0b9b8da9
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-27 09:19:15 +0300

    yo

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
 
 function ToDo(props) {
+  const [check, setCheck] = useState(props.todo.completed);
+
+  function handleCheck(event){
+    setCheck(event.target.checked); 
+  }
+
     return (
-      <p>
-        Ok
-      </p>
+      <>
+    <p>{props.todo.title}</p>
+    <p>userID: {props.todo.userId}</p>
+    <p>toDoId: {props.todo.id}</p>
+    <input type="checkbox" checked={checked} onChange={handleCheck}></input>
+    </>
     );
   }
   
